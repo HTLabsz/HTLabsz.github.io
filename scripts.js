9fe86cf6c28efae08023a497ce763825c6a543dc
HTLabsz <leonardolatzkee@gmail.com>
2024-09-17 21:56:41 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('nav a[href^="#"]');
+    const header = document.querySelector('header');
+
+    // Adicionar rolagem suave
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop - header.offsetHeight,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+
+    // Alterar cor do cabeçalho ao rolar
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 0) {
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    });
+});
